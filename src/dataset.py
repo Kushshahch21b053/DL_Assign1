7c93b147120a98571413b81c4600e6e62ddd59a9
Kush <kush.rushabh@gmail.com>
2025-03-09 16:35:23 +0530

    Modified src/dataset.py for Question 1

--- src/dataset.py
+++ src/dataset.py
@@ -3,6 +3,7 @@ os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
 
 import numpy as np
 import wandb
+import matplotlib.pyplot as plt
 
 from keras.datasets import fashion_mnist
 
@@ -19,14 +20,25 @@ def log_fashion_mnist_samples():
     class_labels = ["T-shirt/top", "Trouser", "Pullover", "Dress", "Coat", "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"]
 
     for step in range(5):
-        images = []
-        for class_index in range(10):
-            indices = np.where(y_train == class_index)[0]
-            random_index = np.random.choice(indices)
-            caption = f"{class_labels[class_index]} (step={step})"
-            images.append(wandb.Image(x_train[random_index], caption=caption))
+        fig, axes = plt.subplots(4, 3, figsize=(14, 12))
+        axes = axes.flatten()
+        for i, class_index in enumerate(range(10)):
+            idxs = np.where(y_train == class_index)[0]
+            random_idx = np.random.choice(idxs)
+            axes[i].imshow(x_train[random_idx], cmap="gray")
+            axes[i].set_title(f"{class_labels[class_index]}", fontsize=24)
+            axes[i].axis("off")
 
-        wandb.log({"examples": images}, step=step)
+        for i in range(10, 12):
+            axes[i].axis("off")
+
+        fig.tight_layout()
+
+        # Convert the entire collage (figure) into one wandb.Image
+        wandb.log({"examples": wandb.Image(fig)}, step=step)
+
+        # Close figure to free memory
+        plt.close(fig)
 
     wandb.finish()
 
